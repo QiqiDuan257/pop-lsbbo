c8053d6f4d7d70fce1bb8e6b384ccd67cc15ba7f
DQQ-077-SZ <duanqq257@qq.com>
2019-08-29 14:45:23 +0800

    plot contour for warga

---
+++ matlab/test/func/base/warga/check/plotContour.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+%%
+figure(1);
+popPlotContour('popFuncWarga', -1 : 0.01 : 1, -1 : 0.01 : 1);
+
+%%
+figure(2);
+popPlotContour('popFuncWarga', -1 : 0.01 : 1, -1 : 0.01 : 1, -1 : 0.5 : 3);
