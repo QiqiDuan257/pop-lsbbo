499fd2718e53a87ce14d7cb828f9ba63022529f1
DQQ-077-SZ <duanqq257@qq.com>
2019-08-29 14:48:50 +0800

    add util: popGetLibInfo.m

---
+++ matlab/main/utils/lib-info/popGetLibInfo.m
@@ -0,0 +1,10 @@
+function libInfo = popGetLibInfo()
+% Get the Library Information.
+libInfo.name = 'pop-lsbbo';
+libInfo.github = 'https://github.com/QiqiDuan257/pop-lsbbo';
+libInfo.version = '0.00.000.1';
+libInfo.author = 'Qiqi Duan and Chang Shao';
+libInfo.contact = 'duanqq257@qq.com';
+libInfo.copyright = 'https://github.com/QiqiDuan257/pop-lsbbo/blob/master/LICENSE';
+libInfo.createDate = 'August 18, 2019 (8-18-2019)';
+end
