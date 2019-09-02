ca5cbe41c262b259e22ff9dd1c95baa87a92b07f
DQQ-077-SZ <duanqq257@qq.com>
2019-09-02 20:34:21 +0800

    add tool: popCheckGradSBFunc.m

---
+++ matlab/main/func/base/miscellaneous-tools/popCheckGradSBFunc.m
@@ -0,0 +1,28 @@
+function popCheckGradSBFunc(funcName, X, G, tol)
+% Check the Gradient of Shifted Base Function via test cases.
+%
+% Input ->
+%   funcName : function name (char or string)
+%   X        : test cases (matrix of size [funcDim * popSize])
+%   G        : gradients (matrix of size [funcDim * popSize])
+%   tol      : error tolerance (scalar),
+%              optional, default: 1e-24
+if nargin < 4
+    tol = 1e-24;
+end
+
+[funcDim, popSize] = size(X);
+if any(size(X) ~= size(G))
+    error('two matrices should have the same size.');
+end
+
+for t = 1 : popSize
+    XX = X(:, t) + popGetShiftVect(X(:, t));
+    if ~popCheckIsClose(feval(funcName, XX), G(:, t), tol) || ...
+            ~popCheckIsClose(feval(funcName, XX'), G(:, t)', tol)
+        error('%d-d: check %s.', funcDim, funcName);
+    end
+end
+
+fprintf('%s: pass the test.\n', funcName);
+end
