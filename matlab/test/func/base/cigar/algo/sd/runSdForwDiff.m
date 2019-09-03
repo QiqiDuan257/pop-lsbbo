274f6103416f5c7f5a7ac1e4ab633eac65a6afdd
DQQ-077-SZ <duanqq257@qq.com>
2019-09-03 16:35:17 +0800

    run SD with forward differencing on cigar

---
+++ matlab/test/func/base/cigar/algo/sd/runSdForwDiff.m
@@ -0,0 +1,42 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <runSdForwDiff.m >runSdForwDiff.out 2>&1 &
+
+%% load the `minFunc` library, which is downloaded from
+%   https://www.cs.ubc.ca/~schmidtm/Software/minFunc.html
+%   https://www.cs.ubc.ca/~schmidtm/Software/copyright.html
+%   https://www.cs.ubc.ca/~schmidtm/Software/minFunc_2012.zip
+% first put runSdGrad.m in the working directory of the `minFunc` library
+addpath(genpath(pwd));
+
+%%
+startRun = tic;
+randSeed = 20190818; % for reproducibility
+funcName = @popFuncGradCigar_;
+funcDims = [2 10 100 1000 2000 4000 6000 8000 10000]';
+% funcDims = [2 10]';
+optRes = cell(length(funcDims), 1);
+for d = 1 : length(funcDims)
+    startRunD = tic;
+    funcDim = funcDims(d);
+    maxFEval = 1e5 * funcDim;
+    rng(randSeed + d);
+    x0 = -100 + 100 * rand(funcDim, 1);
+    options = [];
+    options.Method = 'sd';
+    options.numDiff = 1; % forward-differencing
+    options.MaxFunEvals = maxFEval;
+    options.MaxIter = maxFEval;
+    options.Display = 'off';
+    [x, y, exitFlag, output] = minFunc(funcName, x0, options);
+    res = {};
+    res.opty = y;
+    res.optx = x;
+    res.exitFlag = exitFlag;
+    res.output = output;
+    optRes{d} = res;
+    fprintf(sprintf('%6d: opty %7.2e + runtime %7.2e.\n', ...
+        funcDim, y, toc(startRunD)));
+end
+save('runSdForwDiff.mat', 'optRes', 'funcDims', '-v7.3');
+fprintf(sprintf('Total runtime: %7.5e.\n', toc(startRun)));
