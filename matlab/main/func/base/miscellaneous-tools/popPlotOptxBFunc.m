f93a13de4b56918112a3efdf242dc0a3e86043da
DQQ-077-SZ <duanqq257@qq.com>
2019-09-03 14:33:22 +0800

    add tool: popPlotOptxBFunc.m

---
+++ matlab/main/func/base/miscellaneous-tools/popPlotOptxBFunc.m
@@ -0,0 +1,15 @@
+function popPlotOptxBFunc(optRes, funcDims)
+% Plot Optx obtained on the Base Function.
+if length(optRes) ~= length(funcDims)
+    error('`length(optRes)` should equal `length(funcDims)`.');
+end
+startRun = tic;
+fprintf([popRepChar(14) ' *** plot optx *** ' popRepChar(14) '\n']);
+for d = 1 : length(optRes)
+    subplot(3, 3, d);
+    plot(optRes{d}.optx, 'm.');
+    title(funcDims(d));
+    xlabel('Dimension index'); ylabel('optx');
+end
+fprintf(sprintf('Total runtime: %7.5e.\n', toc(startRun)));
+end
