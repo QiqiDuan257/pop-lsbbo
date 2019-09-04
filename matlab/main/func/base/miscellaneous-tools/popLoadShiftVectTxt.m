0eb7d6dffe90dbb2d8375392e8c12a1e921f4b3a
DQQ-077-SZ <duanqq257@qq.com>
2019-09-04 14:52:45 +0800

    add tool: popLoadShiftVectTxt.m

---
+++ matlab/main/func/base/miscellaneous-tools/popLoadShiftVectTxt.m
@@ -0,0 +1,24 @@
+function shift = popLoadShiftVectTxt(funcDim)
+% Load the Shift Vector Txt file.
+% Input ->
+%   funcDim : function dimension (positive integer)
+% Output <-
+%   shift   : shift vector (column vector)
+if ~popCheckIsPosInt(funcDim)
+    error('`funcDim` should be a positive integer.');
+end
+folder = sprintf('/.popInputData/popShiftVector%d.txt', funcDim);
+if exist(['.' folder], 'file')
+    folder = ['.' folder];
+elseif exist(['..' folder], 'file')
+    folder = ['..' folder];
+elseif exist(['../..' folder], 'file')
+    folder = ['../..' folder];
+else
+    error('cannot find the shift vector TXT file.');
+end
+shift = load(folder);
+if any(size(shift) ~= [funcDim 1])
+    error('incorrectly load input data -> shift vector.');
+end
+end
