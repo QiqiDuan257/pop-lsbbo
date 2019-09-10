50c45810fa4f893206e5b87e39138886734f9d22
DQQ-077-SZ <duanqq257@qq.com>
2019-09-10 20:56:40 +0800

    add tool: popCheckIsClose.m

---
+++ matlab/main/utils/miscellaneous-tools/popCheckIsClose.m
@@ -0,0 +1,21 @@
+function isClose = popCheckIsClose(X, Y, tol)
+% Check whether a matrix Is Closed to another within the error toleration.
+%
+% Input ->
+%   X       : matrix
+%   Y       : matrix
+%   tol     : error toleration (scalar),
+%             optional, default: 1e-24
+% Output <-
+%   isClose : true (1) or false (0)
+if any(size(X) ~= size(Y))
+    error('two matrices (`X` and `Y`) should have the same size.');
+end
+
+if nargin < 3
+    tol = 1e-24;
+end
+
+isClose = abs(X - Y) < tol;
+isClose = all(isClose(:));
+end
