64734106e0e1a8cd85507d5d62ae8f4d4e8c9c09
DQQ-077-SZ <duanqq257@qq.com>
2019-09-11 14:06:14 +0800

    run geneCommRotations.m

---
+++ matlab/test/func/base/cigar/rotation/.popInputData/geneCommRotations.m
@@ -0,0 +1,7 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <geneCommRotations.m >geneCommRotations.out 2>&1 &
+
+%%
+popGeneCommRotations(20190906, popGeneCommFuncDims());
+% popGeneCommRotations(20190906, 3 : 7);
