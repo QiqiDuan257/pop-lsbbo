3ab6f6f7e8efa6748dc67c08e6a9e8a7ddb78090
DQQ-077-SZ <duanqq257@qq.com>
2019-09-19 22:27:53 +0800

    run checkCommRotations.m

---
+++ matlab/test/func/base/discus/rotation/.popInputData/checkCommRotations.m
@@ -0,0 +1,7 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <checkCommRotations.m >checkCommRotations.out 2>&1 &
+
+%%
+popCheckCommRotations(popGeneCommFuncDims());
+% popCheckCommRotations(3 : 7);
