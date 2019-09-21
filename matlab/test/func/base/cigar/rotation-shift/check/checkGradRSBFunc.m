e02d7b25e61fbe29587f159d029929ae4c92225c
DQQ-077-SZ <duanqq257@qq.com>
2019-09-21 22:13:45 +0800

    check gradient of rotated shifted cigar

---
+++ matlab/test/func/base/cigar/rotation-shift/check/checkGradRSBFunc.m
@@ -0,0 +1,8 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <checkGradRSBFunc.m >checkGradRSBFunc.out 2>&1 &
+
+funcName = 'popRSGradCigar_';
+
+%%
+popCheckCommGradRSBFunc(funcName);
