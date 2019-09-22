1cf969bd0d4e6a1f46414b7d78ad090c388c22a7
DQQ-077-SZ <duanqq257@qq.com>
2019-09-22 20:36:31 +0800

    run BFGS without gradient on shifted discus

---
+++ matlab/test/func/base/discus/shift/algo/bfgs/runBfgsNoGrad.m
@@ -0,0 +1,37 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <runBfgsNoGrad.m >runBfgsNoGrad.out 2>&1 &
+
+%%
+startRun = tic;
+randSeed = 20190819; % for reproducibility
+funcName = 'popSFuncDiscus_';
+funcDims = [2 10 100 1000 2000 4000 6000 8000 10000]';
+% funcDims = [2 10]';
+optRes = cell(length(funcDims), 1);
+for d = 1 : length(funcDims)
+    startRunD = tic;
+    funcDim = funcDims(d);
+    maxFEval = 1e5 * funcDim;
+    rng(randSeed + d);
+    x0 = -100 + 100 * rand(funcDim, 1);
+    options = optimoptions(@fminunc, ...
+        'Algorithm', 'quasi-newton', ...
+        'SpecifyObjectiveGradient', false, ...
+        'MaxFunctionEvaluations', maxFEval, ...
+        'MaxIterations', maxFEval, ...
+        'ObjectiveLimit', 1e-10, ...
+        'Display', 'off');
+    [x, y, exitFlag, output, grad] = fminunc(funcName, x0, options);
+    res = {};
+    res.opty = y;
+    res.optx = x;
+    res.exitFlag = exitFlag;
+    res.output = output;
+    res.grad = grad;
+    optRes{d} = res;
+    fprintf('%6d: opty %7.2e + runtime %7.2e.\n', ...
+        funcDim, y, toc(startRunD));
+end
+save('runBfgsNoGrad.mat', 'optRes', 'funcDims', '-v7.3');
+fprintf('Total runtime: %7.5e.\n', toc(startRun));
